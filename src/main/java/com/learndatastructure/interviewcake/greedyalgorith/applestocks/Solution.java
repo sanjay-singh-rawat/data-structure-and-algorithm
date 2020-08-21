cefa213f5f8ad4f166112fd5cb5160636e48c4ce
Sanjay Singh Rawat <s.rawat1339@gmail.com>
2020-08-21 13:54:56 +0530

    greedy algorithm : apple stocks

---
+++ src/main/java/com/learndatastructure/interviewcake/greedyalgorith/applestocks/Solution.java
@@ -0,0 +1,109 @@
+package com.learndatastructure.interviewcake.greedyalgorith.applestocks;
+
+import com.learndatastructure.utils.JUnitUtils;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+/**
+ * @author Sanjay Singh Rawat
+ * @since 2020.08.21
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        JUnitUtils.launch(Solution.class);
+    }
+
+    /**
+     * Time Complexity - O(n)
+     * Space Complexity - O(1)
+     *
+     * @param stockPrices
+     * @return the max profit
+     */
+    public static int getMaxProfit(int[] stockPrices) {
+        if (stockPrices.length < 2) {
+            throw new IllegalArgumentException("Getting a profit requires at least 2 prices");
+        }
+
+        // we'll greedily update minPrice and maxProfit, so we initialize
+        // them to the first price and the first possible profit
+        int minPrice = stockPrices[0];
+        int maxProfit = stockPrices[1] - stockPrices[0];
+
+        // start at the second (index 1) time
+        // we can't sell at the first time, since we must buy first,
+        // and we can't buy and sell at the same time!
+        // if we started at index 0, we'd try to buy *and* sell at time 0.
+        // this would give a profit of 0, which is a problem if our
+        // maxProfit is supposed to be *negative*--we'd return 0.
+        for (int currentTime = 1; currentTime < stockPrices.length; currentTime++) {
+            int currentPrice = stockPrices[currentTime];
+
+            // see what our profit would be if we bought at the
+            // min price and sold at the current price
+            int potentialProfit = currentPrice - minPrice;
+
+            // update maxProfit if we can do better
+            maxProfit = Math.max(maxProfit, potentialProfit);
+
+            // update minPrice so it's always
+            // the lowest price we've seen so far
+            minPrice = Math.min(minPrice, currentPrice);
+        }
+        return maxProfit;
+    }
+
+    // tests
+
+    @Test
+    public void priceGoesUpThenDownTest() {
+        final int actual = getMaxProfit(new int[] {1, 5, 3, 2});
+        final int expected = 4;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void priceGoesDownThenUpTest() {
+        final int actual = getMaxProfit(new int[] {7, 2, 8, 9});
+        final int expected = 7;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void priceGoesUpAllDayTest() {
+        final int actual = getMaxProfit(new int[] {1, 6, 7, 9});
+        final int expected = 8;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void priceGoesDownAllDayTest() {
+        final int actual = getMaxProfit(new int[] {9, 7, 4, 1});
+        final int expected = -2;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void priceStaysTheSameAllDayTest() {
+        final int actual = getMaxProfit(new int[] {1, 1, 1, 1});
+        final int expected = 0;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void exceptionWithOnePriceTest() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            getMaxProfit(new int[] {5});
+        });
+    }
+
+    @Test
+    public void exceptionWithEmptyPricesTest() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            getMaxProfit(new int[] {});
+        });
+    }
+}
