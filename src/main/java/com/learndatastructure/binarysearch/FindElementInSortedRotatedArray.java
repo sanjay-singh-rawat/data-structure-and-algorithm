3ba038da77588aefbb9aad977f599d560d7dadf7
Sanjay Singh Rawat <s.rawat1339@gmail.com>
2023-04-19 16:48:36 +0530

    binary-search: find element in sorted rotated array in single iteration

--- src/main/java/com/learndatastructure/binarysearch/FindElementInSortedRotatedArray.java
+++ src/main/java/com/learndatastructure/binarysearch/FindElementInSortedRotatedArray.java
@@ -16,6 +16,9 @@ public class FindElementInSortedRotatedArray {
     public static void main(String[] args) {
         Assertions.assertTrue(findElement(new int[] {11, 12, 15, 18, 2, 5, 6, 8}, 12));
         Assertions.assertFalse(findElement(new int[] {11, 12, 15, 18, 2, 5, 6, 8}, 1));
+
+        Assertions.assertTrue(searchInSingleIteration(new int[] {11, 12, 15, 18, 2, 5, 6, 8}, 12));
+        Assertions.assertFalse(searchInSingleIteration(new int[] {11, 12, 15, 18, 2, 5, 6, 8}, 1));
     }
 
     public static boolean findElement(int[] arr, int target) {
@@ -66,6 +69,34 @@ public class FindElementInSortedRotatedArray {
                 end = mid - 1;
             }
         }
+        return false;
+    }
+
+    private static boolean searchInSingleIteration(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] == target) {
+                return true;
+            }
+            if (arr[mid] <= arr[end]) {
+                if (target > arr[mid] && target <= arr[end]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            } else if (arr[mid] >= arr[start]) {
+                if (target < arr[mid] && target >= arr[start]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+        }
+
         return false;
     }
 }
